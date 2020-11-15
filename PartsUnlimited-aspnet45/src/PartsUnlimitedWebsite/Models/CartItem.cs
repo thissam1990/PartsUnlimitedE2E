78e39f579b5582850d5bb4518e55ebb9532bbe23
Thissam Basha <thissambasha@gmail.com>
2020-11-15 12:00:45 +0000

    my commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My FIRST CHANGE
+        //My \second CHANGE
         [Key]
         public int CartItemId { get; set; }
 
